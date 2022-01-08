67bf929122369c082276bc05144383b3ee2e56bd
smrithiadyant <97330678+smrithiadyant@users.noreply.github.com>
2022-01-08 15:18:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,10 +2,13 @@
 <html>
 <body>
 
+<h1>My first PHP page</h1>
+  
 <?php
-ECHO "Hello World!<br>";
-echo "Hello World!<br>";
-EcHo "Hello World!<br>";
+$color = "red";
+echo "My car is " . $color . "<br>";
+echo "My house is " . $COLOR . "<br>";
+echo "My boat is " . $coLOR . "<br>";
 ?>
 
 </body>
